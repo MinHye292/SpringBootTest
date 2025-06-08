5507fca956a8f168f72a7a62b0057d87fad9b5f9
MinHye292 <8547364@naver.com>
2025-06-08 15:15:09 +0900

    Create Book api 생성

--- demo/demo/src/main/java/com/example/demo/BookController.java
+++ demo/demo/src/main/java/com/example/demo/BookController.java
@@ -1,8 +1,8 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,5 +15,12 @@ public class BookController {
     @GetMapping
     public List<Book> getBooks() {
         return books; // json 배열 형태로 데이터를 받음
+    }
+
+    @PostMapping
+    public ResponseEntity<Book> createBook(@RequestBody Book book) {
+        book.setId((long)(books.size()*1));
+        books.add(book);
+        return ResponseEntity.status(HttpStatus.CREATED).body(book);
     }
 }
